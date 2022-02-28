286a414461d06efe09d26d061bca2a1b44f52bb1
tiagoa-ss <tsousa@alunos.utfpr.edu.br>
2022-02-28 14:55:13 -0300

    Cors  error solved

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
 	res.json({ message: 'Hello' })
