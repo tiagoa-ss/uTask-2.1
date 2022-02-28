bcd3b026c0deb30787c4323052296bb563060518
tiagoa-ss <tsousa@alunos.utfpr.edu.br>
2022-02-28 11:20:11 -0300

    Security update

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 app.use(express.json())
@@ -11,9 +12,13 @@ app.get('/', (req, res) => {
 const taskRoutes = require('./routes/taskRoutes')
 app.use('/task', taskRoutes)
 
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+console.log(DB_USER, DB_PASSWORD)
+
 mongoose
 	.connect(
-		'mongodb+srv://tiagoa-ss:tIag0000@utaskcluster.vjcq1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+		`mongodb+srv://${DB_USER}:${DB_PASSWORD}@utaskcluster.vjcq1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
 	)
 	.then(() => {
 		console.log('App rodando')
